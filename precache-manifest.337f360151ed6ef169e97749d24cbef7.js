cb60e76b0cea4f1e1203108b73bb4e5550726b9f
tsaristbomba <andrexerdna@gmail.com>
2020-04-13 14:54:43 -0300

    Updates

---
+++ precache-manifest.337f360151ed6ef169e97749d24cbef7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2535298c6be5ce9ecaf0243edc211eac",
+    "url": "/temp-chart/index.html"
+  },
+  {
+    "revision": "c7a80bc78aa00e15fc83",
+    "url": "/temp-chart/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "c7a80bc78aa00e15fc83",
+    "url": "/temp-chart/static/js/2.038d5cda.chunk.js"
+  },
+  {
+    "revision": "b4cabae32341920cfb4131dbdebda2c2",
+    "url": "/temp-chart/static/js/2.038d5cda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "586a2dfb00e9330f4331",
+    "url": "/temp-chart/static/js/main.0465cdc6.chunk.js"
+  },
+  {
+    "revision": "0c3d5b5d8b31f66219e7",
+    "url": "/temp-chart/static/js/runtime-main.f1757d95.js"
+  },
+  {
+    "revision": "ee5d39dfaead44bdc88e86d9c56d198d",
+    "url": "/temp-chart/static/media/ZonAnn.Ts+dSST.ee5d39df.csv"
+  }
+]);
